a3c2c12d43d6d40ac29276d4c337f3bd5d71f077
rorybot <29726065+rorybot@users.noreply.github.com>
2017-08-28 15:00:36 +0100

    Working fizzbuzz with test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,31 @@
 require 'fizzbuzz'
 
+
 describe 'fizzbuzz' do
-  it 'returns "fizz" when pass 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
-  end
+
+    it 'returns "fizz" when pass multiple of 3' do
+      expect(fizzbuzz(3)).to eq 'fizz'
+    end
+
+    it 'returns "buzz" when pass multiple of 5' do
+      expect(fizzbuzz(5)).to eq 'buzz'
+    end
+
+
+fizzbuzzers = [15,30,45,60,75,90,105]
+  fizzbuzzers.each{ |i|
+    it 'returns "fizzbuzz" when passed multiple of 3 and 5' do
+      expect(fizzbuzz(i)).to eq 'fizzbuzz'
+    end
+}
+
+
+values = [1,2,4,7,8,11,13,14]
+  values.each{|i|
+      it 'returns #{i} when passed anything but 3' do
+
+      expect(fizzbuzz(i)).to eq i
+        end
+      }
+
 end
